f5d8fcbd657ac000df4fe60a8e456b76ffce5d7b
MityaDSCH <mlsgearon@gmail.com>
2016-02-21 18:07:11 -0600

    initial setup done

---
+++ entry.js
@@ -0,0 +1,5 @@
+'use strict';
+require('!style!css!sass!./index.scss');
+require('d3');
+
+d3.select('body').append('h1').text('Hello World!');
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          cacheDirectory: true
+        }
+      }
+    ]
+  }
+}
