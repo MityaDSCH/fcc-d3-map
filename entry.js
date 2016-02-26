1bc6157580967dd748b274fe5819f071e4f5f2de
MityaDSCH <mlsgearon@gmail.com>
2016-02-25 20:30:44 -0600

    Displays data

--- entry.js
+++ entry.js
@@ -1,5 +1,18 @@
 'use strict';
 require('!style!css!sass!./index.scss');
-require('d3');
 
-d3.select('body').append('h1').text('Hello World!\nEmmia\'s Fat!');
+const body = d3.select('body');
+const width = document.body.clientWidth;
+const height = document.body.clientHeight;
+const svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+d3.json("data/topo.json", (error, data) => {
+  if (error) return console.error(error);
+
+  console.log(data);
+  svg.append("path")
+      .datum(topojson.feature(data, data.objects.subunits))
+      .attr("d", d3.geo.path().projection(d3.geo.conicEquidistant()));
+});
